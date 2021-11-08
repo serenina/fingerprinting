6df3d76c723b0fed53d01843f8371f52576c7031
Serena Della Pina <serena.dellapina@carexs.com>
2021-11-08 11:26:03 +0100

    Updated libraries and added logs

--- fingerprinting/file_type.py
+++ fingerprinting/file_type.py
@@ -1,3 +1,6 @@
+import click
+import logging
+
 signatures = {'PK': 'Microsft Office Document',
               'JFIF': 'JPEG',
               'GIF': 'GIF',
@@ -8,16 +11,30 @@ signatures = {'PK': 'Microsft Office Document',
               'PDF': 'PDF',
               }
 
+logging.basicConfig(
+    format='[%(levelname)s] %(message)s',
+    level=logging.INFO
+)
+logger = logging.getLogger('file_fingerprinting')
 
+
+@click.command()
+@click.option('--path', type=str, default=None, help='local directory to the file')
 def file_fingerprinting(path):
     try:
+        logger.log(logging.INFO, 'Reading file')
         with open(path, 'rb') as f:
             first_bytes = f.read(100)
             for key in signatures.keys():
                 if key in str(first_bytes):
-                    return (signatures[key])
+                    logger.log(logging.INFO, f'File extension: {signatures[key]}')
                 else:
                     continue
             return None
     except FileNotFoundError:
+        logger.log(logging.ERROR, 'FileNotFoundError')
         pass
+
+
+if __name__ == '__main__':
+    file_fingerprinting()
